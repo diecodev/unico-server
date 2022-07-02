0d0c707d4abd6f012806979b9e637e1c040f99c8
Diego Díaz <didimar2001@gmail.com>
2022-07-02 01:34:23 -0500

    Add some deps

---
+++ deps.ts
@@ -0,0 +1,7 @@
+export { Application, Context, Router, } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
+export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
+
+export { create, verify, decode } from "https://deno.land/x/djwt@v1.9/mod.ts";
+
+export { config as dotEnv } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
