2c43ad9c091328d911461ee3814c97055bb06781
Diego Díaz <didimar2001@gmail.com>
2022-07-03 19:23:53 -0500

    Create constants

---
+++ constants.ts
@@ -0,0 +1,11 @@
+import type { CookiesSetDeleteOptions } from "./deps.ts"
+
+export const privateKey = new TextEncoder().encode(Deno.env.get("SECRET"));
+
+export const options: CookiesSetDeleteOptions = {
+  httpOnly: true,
+  path: "/",
+  sameSite: "strict",
+  secure: Deno.env.get("ENVIRONMENT") !== "development",
+  maxAge: 60 * 60 * 24 * 31 * 12
+}
