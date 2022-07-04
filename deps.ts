6897b5baff94a7b485d47eaa021955b14ff5b8d3
Diego Díaz <didimar2001@gmail.com>
2022-07-03 19:22:57 -0500

    Add deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,10 @@
-export { Application, Context, Router, } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+export { Application, Context, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+export type { CookiesSetDeleteOptions } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
 export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
 
-export { create, verify, decode } from "https://deno.land/x/djwt@v1.9/mod.ts";
+export { decodeJwt, jwtVerify as verifyJwt, SignJWT as signJwt } from "https://deno.land/x/jose@v4.8.3/index.ts";
 
 export { config as dotEnv } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+
+export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
