c7684b262f20515e53c42958e4622b481e63cf23
Diego Díaz <didimar2001@gmail.com>
2022-07-03 19:23:34 -0500

    Delete router functionability

--- main.ts
+++ main.ts
@@ -1,17 +1,7 @@
-import { Application, Router, dotEnv } from "./deps.ts";
-import { logout } from "./routes/logout.ts";
-import { adminLogin } from "./routes/admin/index.ts";
+import { Application } from "./deps.ts";
+import { router } from "./routes/index.routes.ts";
 
 const PORT = 5000;
-
-const router = new Router();
-
-dotEnv({ export: true });
-
-router
-  .get("/logout", logout) // log out the user
-  .post("/logout", logout) // log out the user too
-  .post("/admin", adminLogin);
 
 const app = new Application();
 app.use(router.routes());
@@ -19,4 +9,3 @@ app.use(router.allowedMethods());
 
 console.log(`Server is running on http://localhost:${PORT}`);
 await app.listen({ port: PORT });
-({ port: PORT });
