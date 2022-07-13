84e193efa17798c166bcae93794a37fc7cb7c98f
Diego Díaz <didimar2001@gmail.com>
2022-07-13 17:08:39 -0500

    Remove dovEnv dep

--- deps.ts
+++ deps.ts
@@ -5,6 +5,4 @@ export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
 
 export { decodeJwt, jwtVerify as verifyJwt, SignJWT as signJwt } from "https://deno.land/x/jose@v4.8.3/index.ts";
 
-export { config as dotEnv } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-
 export * as bcrypt from "https://raw.githubusercontent.com/JamesBroadberry/deno-bcrypt/issue/28/mod.ts";
