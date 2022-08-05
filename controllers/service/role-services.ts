fff2748bb1db7e670e8b0453ae07e9669947e8ca
Diego Díaz <didimar2001@gmail.com>
2022-08-04 21:42:26 -0500

    Create roles services list

---
+++ controllers/service/role-services.ts
@@ -0,0 +1,51 @@
+import { RouterContext, verifyJwt, Bson } from '../../deps.ts';
+import { TokenData } from '../controllers.types.d.ts'
+import { ServiceSchema } from '../../types.d.ts';
+import { privateKey, populateServiceOptions } from '../../constants.ts';
+import { getIntervals } from '../../utils/get-intervals.ts'
+import db from '../../utils/db.ts';
+
+export const roleServices = async (ctx: RouterContext<'/services/:role/:id'>) => {
+  const { response, params, cookies } = ctx;
+
+  const { role } = params;
+  const id = new Bson.ObjectId(params.id);
+
+  const token = await cookies.get('untkca', { signed: true });
+
+  //setting the response type and status.
+  response.status = 401;
+  response.type = 'application/json';
+  try {
+    // if there is not token or if sort is diff than asc or desc, return error.
+    if (!token || (role !== 'clients' && role !== 'cadets' && role !== 'schedulers')) throw new Error('You have not access to this resource.');
+
+    // if there is a token, verify it.
+    const decoded = (await verifyJwt(token, privateKey)).payload as unknown as TokenData;
+
+    // if the user is not an admin or scheduler, return error.
+    if (decoded.role !== ' admin' && decoded.role !== 'agendador') throw new Error('You have not access to this resource.');
+
+    // searching data in db
+    const service_model = db.collection<ServiceSchema>('services');
+
+    // taking dates for query
+    const { first_date } = getIntervals();
+
+    const services = await service_model.aggregate([
+      { $match: { date_of_service: { $lte: first_date }, $or: [{ picked_up_by: id }, { delivered_by: id }, { client_id: id }, { scheduled_by: id }] } },
+      { $sort: { date_of_service: -1 } },
+      ...populateServiceOptions,
+    ]).toArray();
+
+    response.status = 200;
+    response.body = { data: services };
+    return;
+
+  } catch (error) {
+    // if error exist, then return it
+    if (error instanceof Error) return response.body = { error: error.message };
+
+    return response.body = { error: error.toString() };
+  }
+}
