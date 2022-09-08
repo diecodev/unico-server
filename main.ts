8c22747a25ca3464c5645d45c617b33ac18cc5e7
Diego Díaz <didimar2001@gmail.com>
2022-09-08 11:36:11 -0500

    Bug fetch login

--- main.ts
+++ main.ts
@@ -8,6 +8,9 @@ const SECRET_TWO = Deno.env.get('COOKIE_SECRET_TWO') as string;
 const app = new Application({ keys: [SECRET_ONE, SECRET_TWO] });
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.addEventListener('error', (event) => {
+  console.log({ error: event.error });
+})
 
 console.log(`Server is running on http://localhost:${PORT}`);
 await app.listen({ port: PORT });
