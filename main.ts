e32a500f6046e67d9eb3ea6c40754b3ea0d1ffd3
Diego Díaz <didimar2001@gmail.com>
2022-09-20 09:21:32 -0500

    Fix: Reset connection (?)

--- main.ts
+++ main.ts
@@ -6,11 +6,16 @@ const SECRET_ONE = Deno.env.get('COOKIE_SECRET_ONE') as string;
 const SECRET_TWO = Deno.env.get('COOKIE_SECRET_TWO') as string;
 
 const app = new Application({ keys: [SECRET_ONE, SECRET_TWO] });
+const controller = new AbortController();
+
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+// If there is an error, reset the connection
 app.addEventListener('error', (event) => {
-  console.log({ error: event.error });
+  console.log(event.error);
+  controller.abort();
 })
 
 console.log(`Server is running on http://localhost:${PORT}`);
-await app.listen({ port: PORT });
+await app.listen({ port: PORT, signal: controller.signal });
