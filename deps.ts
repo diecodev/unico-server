ce93a1e19a0df959fa4314a720893b77932f9267
Diecodev <diegoddiaz05@gmail.com>
2023-04-14 10:59:55 -0500

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export { Application, Context, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
-export type { CookiesSetDeleteOptions, RouterContext } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+export { Application, Context, Router } from 'https://deno.land/x/oak@v12.1.0/mod.ts';
+export type { CookiesSetDeleteOptions, RouterContext } from 'https://deno.land/x/oak@v12.1.0/mod.ts';
 
-export { Bson, MongoClient } from 'https://deno.land/x/mongo@v0.31.1/mod.ts';
+export { Bson, MongoClient } from 'https://deno.land/x/mongo@v0.31.2/mod.ts';
 
-export { decodeJwt, jwtVerify as verifyJwt, SignJWT as signJwt } from 'https://deno.land/x/jose@v4.9.3/index.ts';
+export { decodeJwt, jwtVerify as verifyJwt, SignJWT as signJwt } from 'https://deno.land/x/jose@v4.13.2/index.ts';
 
-export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
+export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
